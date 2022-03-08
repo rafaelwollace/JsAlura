928014191c8cbcf1f11287400e8076e9af9898f6
rafaelwollace <rafael.wfc@gmail.com>
2022-03-08 08:22:32 -0400

    Primeiro comit

---
+++ js/principal.js
@@ -0,0 +1,38 @@
+var titulo = document.querySelector("h1");
+titulo.textContent = "RAFAEL TITULO";
+
+
+
+
+var pacientes = document.querySelectorAll(".paciente");
+
+for (var i = 0; i < pacientes.length; i++) {
+
+    var paciente = pacientes[i];
+
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+
+    var tdImc = paciente.querySelector(".info-imc");
+
+    var pesoEhValido = true;
+    var alturaEhValida = true;
+
+    if (peso < 0 || peso > 1000) {
+        pesoEhValido = false;
+        tdImc.textContent = 'Peso Invalido';
+    }
+
+    if (altura < 0 || altura > 3.000) {
+        alturaEhValida = false;
+        tdImc.textContent = 'Altura Invalido';
+    }
+
+    if (alturaEhValida && pesoEhValido) {
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    }
+}
